b57a8bfad1ef3145368fe2fa00329ed2eeeff67a
Volker Rath <volker.rath.montblanc@gmail.com>
2024-08-18 20:20:57 +0200

    selective merging

--- aempy/scripts/PRE_merge.py
+++ aempy/scripts/PRE_merge.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: -all
+#     formats: py:light,ipynb
+#     main_language: python
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.16.2
+#   kernelspec:
+#     display_name: Python 3 (Spyder)
+#     language: python3
+#     name: python3
+# ---
+
 """
 Created on Thu Apr 18 08:37:28 2024
 
@@ -17,6 +34,7 @@ import warnings
 import numpy
 
 
+# +
 AEMPYX_ROOT = os.environ["AEMPYX_ROOT"]
 mypath = [os.path.join(AEMPYX_ROOT, "aempy/modules/")]
 for pth in mypath:
@@ -26,40 +44,50 @@ for pth in mypath:
 from version import versionstrg
 import util
 import aesys
+import inverse
 
+# -
 
-OutInfo = True
 AEMPYX_DATA = os.environ["AEMPYX_DATA"]
-
 version, _ = versionstrg()
-titstrng = util.print_title(version=version, fname=__file__, out=False)
+# script = "Tutorial2_PRE_merge.py"
+script = __file__  # this only works in python, not jupyter notebook
+titstrng = util.print_title(version=version, fname=script, out=False)
 print(titstrng+"\n\n")
 
-
-
-
-FileList = "search"  # "search", "read"
-LinesOut = True
-LinesMin = 30
-CheckNaN = True
-MergeOut = True
-
-
+# for tutorial only...
+AEMPYX_DATA = AEMPYX_ROOT+"/data/"
+AEMPYX_DATA =  "/media/vrath/BackMetal/"
 
 FileList = "search"  # "search", "read"
 
 
-InDatDir = AEMPYX_ROOT+"/work/data/dec/"
-OutDatDir = AEMPYX_ROOT+"/work/data/merged/"
+DataType = "models"
+InDatDir = AEMPYX_DATA+"/aem05_mallow/dec/median10/results/"
+OutDatDir = AEMPYX_DATA+"/aem05_mallow/merged/"
+Thresh = ["smp", 10.]
 
-SearchStrng = "*_k2*5*mean.npz"
-OutFileName = OutDatDir+"LimShale_k2_dec5_mean_merged"
-OutHeader =" Limerick Shale project, k2 dec5 mean merged"
+
+SearchStrng = "*k2*gcv*results.npz"
+OutFileName = OutDatDir+"DIG_Mallow_k2_gcv_merged_results.npz"
+OutHeader =" DIG_Mallow_project, k2, gcv,  merged"
+
+# SearchStrng = "*delete_Tikh*gcv*results.npz"
+# OutFileName = OutDatDir+"DIG_Mallow_k8_gcv_merged_results.npz"
+# OutHeader =" DIG_Mallow_project, k8, gcv,  merged"
+
+
+# DataType = "data"
+# InDatDir = AEMPYX_ROOT+"/data/"
+# OutDatDir = AEMPYX_ROOT+"/mallow/merged/"
+# SearchStrng = "*_k2*5*mean.npz"
+# OutFileName = OutDatDir+"LimShale_k2_dec5_mean_merged"
+# OutHeader =" Limerick Shale project, k2 dec5 mean merged"
 
 
 print("Data read from dir:  %s" % InDatDir)
 print("Data written to dir: %s" % OutDatDir)
-print("New flightline Header string: %s" % OutHeader)
+print("New header string: %s" % OutHeader)
 print("SearchStrng is %s\n" % SearchStrng)
 
 
@@ -85,9 +113,14 @@ ns = numpy.size(dat_files)
 if ns ==0:
     error("No files set!. Exit.")
 
-# aesys.merge_data_files(File_list=dat_files, 
-#                     Merged=OutFileName, MergedHeader=OutHeader, 
-#                     OutInfo=False)
-util.merge_data_sets(file_list=dat_files, aem_system="aem05",
+
+if "dat" in DataType.lower():  
+    _ = inverse.merge_data_sets(file_list=dat_files, aem_system="aem05",
+                                       outfile_name=OutFileName,
+                                       out=False)
+else:
+    _ = inverse.merge_model_sets(infile_list=dat_files, thresh=Thresh,
                                    outfile_name=OutFileName,
                                    out=False)
+
+
